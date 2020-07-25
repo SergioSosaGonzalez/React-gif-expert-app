1161f5ccc7f8cbd02372f9dfdeda892e59127452
Sergio Arturo Sosa González <sergioarsosa95@gmail.com>
2020-07-25 17:44:59 -0500

    Docs added

---
+++ docs/precache-manifest.e9b6bf14914fa0ff5651df799ea7af35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ce5e88efcb57821385a0af8c18848d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49a123f7359d70b56dfc",
+    "url": "/static/css/main.5622edf3.chunk.css"
+  },
+  {
+    "revision": "a50d5cb98697f7283138",
+    "url": "/static/js/2.0ba6f1d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0ba6f1d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49a123f7359d70b56dfc",
+    "url": "/static/js/main.ff025b3a.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
